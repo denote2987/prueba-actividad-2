bf9c7708cf7f103ed6c6450940ab6cd3d4932d31
denote2987 <bsurbinag@unincca.edu.co>
2026-04-15 01:17:57 +0000

    Delete layout.js

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	base: "/prueba-actividad-2/",
-	build: {
-    outDir: 'build', // Esto es por defecto, pero asegúrate de que no diga otra cosa
-  }
+	plugins: [sveltekit()]
 });
